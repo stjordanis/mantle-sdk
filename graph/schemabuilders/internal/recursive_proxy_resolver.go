2ca05956cc25e5ef148f900acc505efda923a16d
jess <jesse@terra.money>
2021-01-21 22:28:41 +0900

    resolver

--- graph/schemabuilders/internal/recursive_proxy_resolver.go
+++ graph/schemabuilders/internal/recursive_proxy_resolver.go
@@ -1,33 +1,52 @@
 package internal
 
 import (
+	"bytes"
 	"github.com/graphql-go/graphql"
 )
 
 type ProxyResolverContext struct {
 	Resolver graphql.FieldResolveFn
 	IsSealed bool
-	Children []graphql.FieldResolveFn
 }
+
+type GraphQLSubtreeReconstructorFn func(typeName string, arguments []string)
 
 func NewProxyResolverContext() *ProxyResolverContext {
 	return &ProxyResolverContext{
 		Resolver: nil,
-		Children: make([]graphql.FieldResolveFn, 0),
+		IsSealed: true,
 	}
 }
 
-func (prc *ProxyResolverContext) CreateRootQuery() *ProxyResolverContext {
+func (prc *ProxyResolverContext) CreateResolver() *ProxyResolverContext {
 	if prc.IsSealed {
 		panic("cannot redeclare root resolver once sealed")
 	}
 	prc.IsSealed = true
 	prc.Resolver = func(p graphql.ResolveParams) (interface{}, error) {
+		// is root resolver
+		var buf bytes.Buffer
 
+		// query reconstructor
+		return func(typeName string, arguments []string) {
+
+		}, nil
 	}
 	return prc
 }
 
-func (prc *ProxyResolverContext) CreateSubtree(typeName string) {
+func (prc *ProxyResolverContext) CreateSubtree(typeName string) *ProxyResolverContext {
+
+	var resolver graphql.FieldResolveFn = func(p graphql.ResolveParams) (interface{}, error) {
+		reconstruct, ok := p.Source.(GraphQLSubtreeReconstructorFn)
+		if !ok {
+			panic("source is not a reconstructor fn")
+		}
+
+		reconstruct(p.Info.FieldName, nil)
+	}
+
+	prc.
 
 }
